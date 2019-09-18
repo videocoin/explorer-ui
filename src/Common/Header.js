cff6bcab05d56adec9a7b7ea4d9923b80e7096ad
Dana <dana@liveplanet.net>
2019-09-18 09:55:49 -0700

    Add .env.

--- src/Common/Header.js
+++ src/Common/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Logo } from 'ui-kit';
+import { Logo } from 'ui-kit'
+import css from './header.module.scss'
 
 const Header = () => {
   return (
-    <div>
+    <div className={css.header}>
       <Logo type="colorWhite" width={171} />
+      <h1>Compiled successfully!</h1>
     </div>
   );
 };
