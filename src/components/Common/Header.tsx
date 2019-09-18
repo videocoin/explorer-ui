8b8b41e7aecac0ce08fe12705c384d240c57a62c
Dana <dana@liveplanet.net>
2019-09-18 10:10:56 -0700

    Move components into components dir.

---
+++ src/components/Common/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Logo } from 'ui-kit'
+import css from './header.module.scss'
+
+const Header: React.FC = () => {
+  return (
+    <div className={css.header}>
+      <Logo type="colorWhite" width={171} />
+      <h1>Compiled successfully!</h1>
+    </div>
+  );
+};
+
+Header.defaultProps = {};
+
+export default Header;
