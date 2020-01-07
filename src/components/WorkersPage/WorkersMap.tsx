a300f0b3ec5ef86a573687e25434a7df630fd6da
Alexander Polovnikov <aleksandrpol@gmail.com>
2020-01-07 17:32:58 +0300

    add miners table

--- src/components/WorkersPage/WorkersMap.tsx
+++ src/components/WorkersPage/WorkersMap.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
+import { map } from 'lodash/fp';
 import mapStyle from './mapStyle.json';
-
+import { Worker } from 'types/common';
+import css from './styles.module.scss';
 const apiKey = process.env.REACT_APP_GOOGLE_MAP_KEY;
 
-const WorkersMap = () => {
+const Marker = ({ lat, lng }: { lat: number; lng: number }) => (
+  <div className={css.marker} />
+);
+
+const WorkersMap = ({ data }: { data: Worker[] }) => {
   const props = {
     center: {
       lat: 40,
@@ -17,13 +23,23 @@ const WorkersMap = () => {
     minZoom: 1,
     styles: mapStyle
   };
+  const renderMarker = (item: Worker) => (
+    <Marker
+      key={item.id}
+      lat={item.systemInfo.latitude}
+      lng={item.systemInfo.longitude}
+    />
+  );
+
   return (
     <GoogleMapReact
       bootstrapURLKeys={{ key: apiKey }}
       defaultCenter={props.center}
       defaultZoom={props.zoom}
       options={mapOptions}
-    />
+    >
+      {map(renderMarker)(data as [])}
+    </GoogleMapReact>
   );
 };
 
