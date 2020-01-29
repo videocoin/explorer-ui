e75a8b256a802953a521d48188c4a025f1a8a438
Alexander Polovnikov <aleksandrpol@gmail.com>
2020-01-29 23:18:46 +0300

    add colors for map markers

--- src/const.ts
+++ src/const.ts
@@ -2,3 +2,10 @@ export const BLOCKS_OFFSET = 20;
 export const TRANSACTIONS_OFFSET = 20;
 export const POLL_TIMEOUT = 5000;
 export const IGNORE_OUTSIDE_CLASS_NAME = 'ignore-onclickoutside';
+
+export enum WorkerStatus {
+  OFFLINE = 'OFFLINE',
+  NEW = 'NEW',
+  IDLE = 'IDLE',
+  BUSY = 'BUSY'
+}
